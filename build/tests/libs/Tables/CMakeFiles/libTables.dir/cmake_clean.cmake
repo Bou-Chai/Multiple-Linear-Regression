2063ec5e6189661ab7285069247d6bee9e2092df
Tyseer Ammar Shahriar <115742900+Bou-Chai@users.noreply.github.com>
2025-07-23 17:34:04 -0600

    Delete build directory

--- build/tests/libs/Tables/CMakeFiles/libTables.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "liblibTables.a"
-  "liblibTables.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libTables.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
